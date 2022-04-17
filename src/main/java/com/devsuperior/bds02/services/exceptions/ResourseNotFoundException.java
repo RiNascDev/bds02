c75ea0f62119972a0fdd3663a6e72750a7286818
Rita de Cássia Leite Nascimento <sarahsetej@gmail.com>
2022-04-17 02:48:39 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/ResourseNotFoundException.java
@@ -0,0 +1,12 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class ResourseNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourseNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+
+}
